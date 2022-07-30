e6c14ef6f6fae7adbeb154048846db34a141aab7
rferraro <RFerraro@tandemdiabetes.com>
2022-07-30 07:38:36 -0700

    nvidia-smi

--- src/project_lightning.sh
+++ src/project_lightning.sh
@@ -5,7 +5,6 @@
 #SBATCH -p standard
 #SBATCH --nodes=2
 #SBATCH --gres=gpu:1
-#SBATCH --array=1-2
 #SBATCH --ntasks-per-node=8
 #SBATCH --mem=6G
 #SBATCH --time=0-02:00:00
@@ -27,4 +26,5 @@ export PYTHONFAULTHANDLER=1
 # module load NCCL/2.4.7-1-cuda.10.0
 
 # run script from above
-srun python3 src/project_lightning.py --num_nodes=2 --num_devices=1
+# srun python3 src/project_lightning.py --num_nodes=2 --num_devices=1
+nvidia-smi
